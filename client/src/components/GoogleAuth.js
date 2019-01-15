42247a550198b92094c5063499d533896d691158
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-15 12:44:18 -0500

    get oAuth instance

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
 
 class GoogleAuth extends Component {
+    state = {
+        isSignedIn: null // coz, we don't know if user is signed in or not
+    };
+
     componentDidMount() {
         window.gapi.load("client:auth2", () => {
-            window.gapi.client.init({
-                clientId: process.env.REACT_APP_OAUTH_CLIENT_ID,
-                scope: "email"
-            });
+            window.gapi.client
+                .init({
+                    clientId: process.env.REACT_APP_OAUTH_CLIENT_ID,
+                    scope: "email"
+                })
+                .then(() => {
+                    this.auth = window.gapi.auth2.getAuthInstance();
+                    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                });
         });
     }
 
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return <div>Don't know if signed in!</div>;
+        } else if (this.state.isSignedIn) {
+            return <div>Signed In!!!</div>;
+        } else {
+            return <div>Not Signed In!</div>;
+        }
+    }
+
     render() {
-        return <div>GoogleAuth</div>;
+        return <div>{this.renderAuthButton()}</div>;
     }
 }
 
