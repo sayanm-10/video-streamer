03ab5744fc0b284eb6df4a6eba2138222dacc53a
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-17 15:27:33 -0500

    implement thunk middleware and add streams

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
-import { SIGN_IN, SIGN_OUT } from "./type";
+import streams from "../apis/streams";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./type";
 
 export const signIn = userId => {
     return {
@@ -10,5 +11,14 @@ export const signIn = userId => {
 export const signOut = () => {
     return {
         type: SIGN_OUT
+    };
+};
+
+export const createStream = formValues => async dispatch => {
+    const response = await streams.post("./streams", formValues);
+
+    return {
+        type: CREATE_STREAM,
+        payload: response.data
     };
 };
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends Component {
     // renderInput(formProps) {
@@ -30,9 +32,9 @@ class StreamCreate extends Component {
         }
     }
 
-    onSubmit(formValues) {
-        console.log(formValues);
-    }
+    onSubmit = formValues => {
+        this.props.createStream(formValues);
+    };
 
     render() {
         return (
@@ -72,7 +74,12 @@ const validateForm = formValues => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapper = reduxForm({
     form: "streamCreate",
     validate: validateForm
 })(StreamCreate);
+
+export default connect(
+    null,
+    { createStream }
+)(formWrapper);
