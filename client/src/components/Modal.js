3b34757da02096ea88d2f03992bbb2e4ee91ba7d
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-20 17:47:19 -0500

    style modal and add functionality to dismiss it

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = props => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">Modal!</div>
+        <div
+            className="ui dimmer modals visible active"
+            onClick={() => history.push("/")}
+        >
+            <div
+                className="ui standard modal visible active"
+                onClick={e => e.stopPropagation()}
+            >
+                <div className="header">Delete Stream</div>
+                <div className="content">
+                    Are you sure you want to delete this stream
+                </div>
+                <div className="actions">
+                    <div className="ui button negative">Delete</div>
+                    <div className="ui button">Cancel</div>
+                </div>
+            </div>
         </div>,
         document.querySelector("#modal")
     );
