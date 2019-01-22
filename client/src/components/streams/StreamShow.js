dfca45ae2d58ba92ee58f1849f864952da9568da
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-21 19:55:49 -0500

    implement FLV player

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,48 @@
 import React, { Component } from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends Component {
-    componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+    constructor(props) {
+        super(props);
+
+        this.videoRef = React.createRef();
     }
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+
+        this.props.fetchStream(id);
+        this.buildPlayer();
+
+        // ! This approach doesn't work as videoRef is null in this life cycle event
+        //this.videoRef.current.addEventListener("load", this.buildPlayer);
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer = () => {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        const { id } = this.props.match.params;
+
+        // wire FLV player
+        this.player = flv.createPlayer({
+            type: "flv",
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+    };
 
     render() {
         if (!this.props.stream) {
@@ -16,6 +53,11 @@ class StreamShow extends Component {
 
         return (
             <div>
+                <video
+                    ref={this.videoRef}
+                    style={{ width: "100%" }}
+                    controls={true}
+                />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
